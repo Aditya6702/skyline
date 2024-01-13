e05977fb335b85cb584df5bfa54c4ec28015c305
Sharan K <127182301+SharanK2003@users.noreply.github.com>
2024-01-14 04:09:16 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/Help/About";
+import Projects from "./components/Flight-Home/Projects";
+import FlightTracking from "./FlightTracking"; // Import the FlightTracking component
+import Footer from "./components/Footer";
+import Resume from "./components/catalog/ResumeNew";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Flight-Home" element={<Projects />} />
+          <Route path="/flight-tracking" element={<FlightTracking />} /> {/* Add the FlightTracking route */}
+          <Route path="/help" element={<About />} />
+          <Route path="/catalog" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
